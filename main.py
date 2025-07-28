33179afab7d1b3add2c3d5d882f7a9e1d74d7dc3
shaban <shaban.elaev@mail.ru>
2025-07-28 12:53:52 +0300

    add func: get_start

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+import aiogram
+from aiogram import types
+from aiogram.filters import command
+from aiogram import F
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram import Bot, Dispatcher
+import config
+from config import token
+
+bot = Bot(token=config.token)
+
+dp = Dispatcher()
+
+@dp.message(command.CommandStart())
+async def get_start(message: types.Message):
+
+    await message.reply("""
+Привет, пользователь!
+Этот телеграмм бот поможет тебе выбрать фильм.
+просто введи:
+/recommend <жанр> <год>
+P.S если нужна информация по жанрам и годам введи команду /Info
+    """)
+
